b629460fec8a15fb68e397b7b56cacf9fdc94579
PirxDanford <clemens.scholz@oxid-esales.com>
2019-09-30 11:13:29 +0200

    Remove options for chromedriver and always start chrome

--- codeception/chromedriver/entrypoint.sh
+++ codeception/chromedriver/entrypoint.sh
@@ -1,31 +1,20 @@
 #!/bin/sh
 
-if [ "$ENABLE_WEBDRIVER" = true ] && [ "$DISABLE_X11" = true ]; then
-  exec chromedriver "$@"
-fi
-
-if [ "$ENABLE_WEBDRIVER" = true ]; then
-  chromedriver &
-fi
-
-if [ "$DISABLE_X11" = true ]; then
-  exec "$@"
-fi
-
-if [ "$ENABLE_VNC" = true ]; then
-  set -- vnc-start "$@"
-fi
-
-if [ "$EXPOSE_X11" = true ]; then
-  set -- --listen-tcp "$@"
-fi
+chromedriver &
+# uncomment to start vnc
+# set -- vnc-start "$@"
+# uncomment to expose X11 port
+# set -- --listen-tcp "$@"
 
 # 6000+SERVERNUM is the TCP port Xvfb is listening on:
-SERVERNUM=$(echo "$DISPLAY" | sed 's/:\([0-9][0-9]*\).*/\1/')
+SERVERNUM=0
+SCREEN_WIDTH=1440
+SCREEN_HEIGHT=900
+SCREEN_DEPTH=24
 
 # Options passed directly to the Xvfb server:
 # -ac disables host-based access control mechanisms
 # −screen NUM WxHxD creates the screen and sets its width, height, and depth
 SERVERARGS="-ac -screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_DEPTH}"
 
-exec xvfb-run --server-num "$SERVERNUM" --server-args "$SERVERARGS" "$@"
+exec xvfb-run -a --server-num "$SERVERNUM" --server-args "$SERVERARGS" "google-chrome"
