e56e01bae9ab4abb9498a39cab5841216cb01f89
PirxDanford <clemens.scholz@oxid-esales.com>
2019-10-01 10:09:53 +0200

    Update image for Selenium 3.8.1 to use a multistage build

--- selenium/S3FF69/entry_point.sh
+++ selenium/S3FF69/entry_point.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# IMPORTANT: Change this file only in directory Standalone!
 
 source /opt/bin/functions.sh
 
@@ -13,32 +15,14 @@ if [ ! -z "$SE_OPTS" ]; then
   echo "appending selenium options: ${SE_OPTS}"
 fi
 
+SERVERNUM=$(get_server_num)
+
 rm -f /tmp/.X*lock
 
-SERVERNUM=$(get_server_num)
-env | cut -f 1 -d "=" | sort > asroot
-sudo -E -u seluser -i env | cut -f 1 -d "=" | sort > asseluser
-sudo -E -i -u seluser \
-  $(for E in $(grep -vxFf asseluser asroot); do echo $E=$(eval echo \$$E); done) \
-  DISPLAY=$DISPLAY \
-  xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
+xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
   java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar -enablePassThrough false \
   ${SE_OPTS} &
 NODE_PID=$!
 
 trap shutdown SIGTERM SIGINT
-for i in $(seq 1 10)
-do
-  xdpyinfo -display $DISPLAY >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    break
-  fi
-  echo Waiting xvfb...
-  sleep 0.5
-done
-
-fluxbox -display $DISPLAY &
-
-x11vnc -forever -usepw -shared -rfbport 5900 -display $DISPLAY &
-
 wait $NODE_PID
