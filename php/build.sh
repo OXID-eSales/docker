03db650905ac32a8f99ff3ad499a0a4bec6e0935
PirxDanford <clemens.scholz@oxid-esales.com>
2020-01-22 19:19:59 +0100

    Introduce possibility to build PHP 5.6, fix dependencies for xdebug and memcached

--- php/build.sh
+++ php/build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+if [[ $1 == "5.6" ]]; then
+  DOCKER_BUILD_ARGUMENTS=()
+  DOCKER_BUILD_ARGUMENTS+=('--build-arg PHP_VERSION=5.6')
+  DOCKER_BUILD_ARGUMENTS+=("--build-arg PECL_DEPS='xdebug-2.5.5 memcached-2.2.0'")
+  BUILD="docker build ${DOCKER_BUILD_ARGUMENTS[*]} -t oxidesales/oxideshop-docker-php:5.6 ."
+  echo $BUILD
+  eval $BUILD
+  exit 0
+fi
+
 if [[ $1 == "7.0" ]]; then
-  BUILD="docker build -t oxidesales/oxideshop-docker-php:7.0 ."
+  DOCKER_BUILD_ARGUMENTS=()
+  DOCKER_BUILD_ARGUMENTS+=("--build-arg PECL_DEPS='xdebug-2.9.0 memcached'")
+  BUILD="docker build ${DOCKER_BUILD_ARGUMENTS[*]} -t oxidesales/oxideshop-docker-php:7.0 ."
   echo $BUILD
   eval $BUILD
   exit 0
@@ -19,9 +31,8 @@ fi
 if [[ $1 == "7.2" ]]; then
   DOCKER_BUILD_ARGUMENTS=()
   DOCKER_BUILD_ARGUMENTS+=('--build-arg PHP_VERSION=7.2')
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg RUNTIME_PACKAGE_DEPS='msmtp libfreetype6 libjpeg62-turbo unzip git default-mysql-client sudo rsync liblz4-tool bc iproute2'")
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg BUILD_PACKAGE_DEPS='libcurl4-openssl-dev libjpeg-dev libpng-dev libxml2-dev libmemcached-dev'")
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg PECL_DEPS='xdebug memcached'")
+  DOCKER_BUILD_ARGUMENTS+=("--build-arg RUNTIME_PACKAGE_DEPS='msmtp libfreetype6 libjpeg62-turbo unzip git default-mysql-client sudo rsync liblz4-tool bc iproute2 libmemcached-dev'")
+  DOCKER_BUILD_ARGUMENTS+=("--build-arg BUILD_PACKAGE_DEPS='libcurl4-openssl-dev libjpeg-dev libpng-dev libxml2-dev zlib1g-dev'")
   BUILD="docker build ${DOCKER_BUILD_ARGUMENTS[*]} -t oxidesales/oxideshop-docker-php:7.2 ."
   echo $BUILD
   eval $BUILD
@@ -31,22 +42,19 @@ fi
 if [[ $1 == "7.3" ]]; then
   DOCKER_BUILD_ARGUMENTS=()
   DOCKER_BUILD_ARGUMENTS+=('--build-arg PHP_VERSION=7.3')
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg RUNTIME_PACKAGE_DEPS='msmtp libfreetype6 libjpeg62-turbo unzip git default-mysql-client sudo rsync liblz4-tool libzip-dev bc iproute2'")
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg BUILD_PACKAGE_DEPS='libcurl4-openssl-dev libjpeg-dev libpng-dev libxml2-dev libmemcached-dev'")
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg PECL_DEPS='xdebug memcached'")
+  DOCKER_BUILD_ARGUMENTS+=("--build-arg RUNTIME_PACKAGE_DEPS='msmtp libfreetype6 libjpeg62-turbo unzip git default-mysql-client sudo rsync liblz4-tool libzip-dev bc iproute2 libmemcached-dev'")
+  DOCKER_BUILD_ARGUMENTS+=("--build-arg BUILD_PACKAGE_DEPS='libcurl4-openssl-dev libjpeg-dev libpng-dev libxml2-dev'")
   BUILD="docker build ${DOCKER_BUILD_ARGUMENTS[*]} -t oxidesales/oxideshop-docker-php:7.3 ."
   echo $BUILD
   eval $BUILD
   exit 0
 fi
 
-# NOTICE: only for internal use, do not publish on dockerhub until 7.4 is released as final
 if [[ $1 == "7.4" ]]; then
   DOCKER_BUILD_ARGUMENTS=()
   DOCKER_BUILD_ARGUMENTS+=('--build-arg PHP_VERSION=7.4')
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg RUNTIME_PACKAGE_DEPS='msmtp libfreetype6 libjpeg62-turbo unzip git default-mysql-client sudo rsync liblz4-tool libzip-dev bc iproute2 libonig-dev'")
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg BUILD_PACKAGE_DEPS='libcurl4-openssl-dev libjpeg-dev libpng-dev libxml2-dev libmemcached-dev'")
-  DOCKER_BUILD_ARGUMENTS+=("--build-arg PECL_DEPS='xdebug-beta memcached'")
+  DOCKER_BUILD_ARGUMENTS+=("--build-arg RUNTIME_PACKAGE_DEPS='msmtp libfreetype6 libjpeg62-turbo unzip git default-mysql-client sudo rsync liblz4-tool libzip-dev bc iproute2 libmemcached-dev libonig-dev'")
+  DOCKER_BUILD_ARGUMENTS+=("--build-arg BUILD_PACKAGE_DEPS='libcurl4-openssl-dev libjpeg-dev libpng-dev libxml2-dev'")
   DOCKER_BUILD_ARGUMENTS+=("--build-arg GD_CONFIG='--with-jpeg=/usr/local/'")
   BUILD="docker build ${DOCKER_BUILD_ARGUMENTS[*]} -t oxidesales/oxideshop-docker-php:7.4 ."
   echo $BUILD
