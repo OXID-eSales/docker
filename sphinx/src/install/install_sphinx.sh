22afe5bc9efbd0cd9d61b2737b9cad9039cab365
Jörn Ott <joern.ott@ott-consult.de>
2023-09-19 12:31:27 +0200

    sphinxcontrib_phpdomain is not yet in stable

--- sphinx/src/install/install_sphinx.sh
+++ sphinx/src/install/install_sphinx.sh
@@ -5,7 +5,6 @@ apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends locales
 locale-gen en_US.utf8
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-cat /etc/locale.gen
 dpkg-reconfigure --frontend=noninteractive locales
 mkdir -p /usr/share/man/man1
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
@@ -24,10 +23,10 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     python3-sphinx \
     python3-rtd-theme \
     python3-sphinxcontrib.httpdomain \
-    python3-sphinxcontrib.plantuml \
-    python3-sphinxcontrib.phpdomain
+    python3-sphinxcontrib.plantuml
 apt-get clean
 apt-get autoremove -y
+pip install sphinxcontrib_phpdomain
 pip install pygments-graphql
 pip install git+https://github.com/fabpot/sphinx-php.git
 java -jar ./usr/share/plantuml/plantuml.jar -testdot
