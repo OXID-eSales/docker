f7e23debb5c0e1057e6264517a81bc5215174e4b
Jörn Ott <joern.ott@ott-consult.de>
2023-09-19 11:59:23 +0200

    Move wheel and docutils from pip install to apt-get

--- sphinx/src/install/install_sphinx.sh
+++ sphinx/src/install/install_sphinx.sh
@@ -10,13 +10,23 @@ locale-gen en_US.utf8
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 dpkg-reconfigure --frontend=noninteractive locales
 mkdir -p /usr/share/man/man1
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends git python3-pip liblz4-tool plantuml graphviz default-jre rsync ssh curl apache2-utils
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    git \
+    python3-pip \
+    liblz4-tool \
+    plantuml \
+    graphviz \
+    default-jre \
+    rsync \
+    ssh \
+    curl \
+    apache2-utils \
+    python3-wheel \
+    python3-docutils \
 apt-get clean
 apt-get autoremove -y
-pip install wheel
 pip install -U Sphinx
 pip install sphinx_rtd_theme==1.2.0
-pip install docutils==0.17.1
 pip install sphinxcontrib-httpdomain
 pip install sphinxcontrib_plantuml
 pip install sphinxcontrib_phpdomain
