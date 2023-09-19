e843b5c22bf8c0bb577b03df0ae0d06a8ee475ad
Jörn Ott <joern.ott@ott-consult.de>
2023-09-19 12:14:19 +0200

    Switch sphinx to debian package as well and debug locale

--- sphinx/src/install/install_sphinx.sh
+++ sphinx/src/install/install_sphinx.sh
@@ -2,14 +2,10 @@
 set -e
 set -x
 apt-get update -y
-locale
-export LANG="en_US.utf8"
-export LANGUAGE="en_US.utf8"
-export LC_ALL="en_US.utf8"
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends locales
 locale-gen en_US.utf8
-cat /etc/locale.gen
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+cat /etc/locale.gen
 dpkg-reconfigure --frontend=noninteractive locales
 mkdir -p /usr/share/man/man1
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
@@ -24,10 +20,10 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     curl \
     apache2-utils \
     python3-wheel \
-    python3-docutils
+    python3-docutils \
+    python3-sphinx
 apt-get clean
 apt-get autoremove -y
-pip install -U Sphinx
 pip install sphinx_rtd_theme==1.2.0
 pip install sphinxcontrib-httpdomain
 pip install sphinxcontrib_plantuml
