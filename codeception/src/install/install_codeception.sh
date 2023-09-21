837ba4fe1a07d382d0f0a0f573b8210acb692949
Jörn Ott <joern.ott@ott-consult.de>
2023-09-21 12:24:37 +0200

    use get-chrome-driver for extraction

--- codeception/src/install/install_codeception.sh
+++ codeception/src/install/install_codeception.sh
@@ -5,7 +5,7 @@ set -x
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 apt-get update -qqy
-apt-get -qqy install ${CHROME_VERSION:-google-chrome-stable} unzip \
+apt-get -qqy install ${CHROME_VERSION:-google-chrome-stable} \
     python3-pip python3-venv
 rm /etc/apt/sources.list.d/google-chrome.list
 rm -rf /var/lib/apt/lists/* /var/cache/apt/*
@@ -15,12 +15,17 @@ chmod a+x /opt/bin/*
 python3 -m venv python3_temporary
 source python3_temporary/bin/activate
 python3 -m pip install requests get-chrome-driver
-which get-chrome-driver
-find / -iname get-chrome-driver
-if [ -z "" ]; then
-  CHROME_DRIVER_VERSION=$(get-chrome-driver --stable-version)
+cd /opt/selenium
+if [ -n "${CHROME_DRIVER_VERSION}" ]; then
+  echo "Using explicitly defined chromedriver version ${CHROME_DRIVER_VERSION}"
+  get-chrome-driver --download-version ${CHROME_DRIVER_VERSION} --extract
+else
+    CHROME_DRIVER_VERSION=$(get-chrome-driver --stable-version)
+    echo "Using chromedriver version matching the current chrome version"
+    get-chrome-driver --auto-download --extract
 fi
-URL=$(get-chrome-driver --stable-url)
+find .
+
 # remove venv and pip
 deactivate
 rm -r python3_temporary
@@ -28,10 +33,9 @@ apt -y remove python3-venv python3-pip
 apt-get clean
 apt-get autoremove -y
 # Install the driver
-echo "Using chromedriver version ${CHROME_DRIVER_VERSION} from ${URL}"
-curl -ssJkLo /tmp/chromedriver_linux64.zip "${URL}"
-unzip -v /tmp/chromedriver_linux64.zip -d /opt/selenium
-rm /tmp/chromedriver_linux64.zip
-mv "/opt/selenium/chromedriver" "/opt/selenium/chromedriver-${CHROME_DRIVER_VERSION}"
+DRIVER=$(find /opt/selenium -type f -iname "chromedriver")
+mv ${DRIVER} "/opt/selenium/chromedriver-${CHROME_DRIVER_VERSION}"
 chmod 755 "/opt/selenium/chromedriver-${CHROME_DRIVER_VERSION}"
 ln -fs "/opt/selenium/chromedriver-${CHROME_DRIVER_VERSION}" "/usr/bin/chromedriver"
+DIR=$(dirname "${DRIVER}")
+rm -r "${DIR}"
